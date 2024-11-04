91e429eedf44a068c729cc36041e278ef5dbfcea
evurose23 <151600605+evurose23@users.noreply.github.com>
2024-11-04 14:35:44 +0530

    Add files via upload

---
+++ waffle.js
@@ -0,0 +1,31 @@
+makewaffle(getIcecream);
+
+function makewaffle(callback){
+    setTimeout(() => {
+        console.log("set the table");
+        console.log("waffle is ready")
+        callback()
+    }, 2500);
+};
+
+function getIcecream(){
+    setTimeout(() => {
+        let icecream=false;
+        if(icecream){
+            console.log("got the icecream")
+        }else{
+            console.log("did ot get icecream")
+        }
+    })
+
+}
+async funtcion snack(){
+    try{
+        await makeWaffle();
+        await geticecream();
+        console.log("set up the table")
+    } catch (error) {
+        console.log{`$(error) so make chocolate syrup`}
+    }
+}
+snack()
